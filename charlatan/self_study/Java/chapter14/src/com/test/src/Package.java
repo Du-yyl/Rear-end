f4275eaa43a49c6647e78d807a3225d222120844
charlatan <yllyou333@163.com>
2022-05-06 18:28:51 +0800

    包管理

--- charlatan/self_study/Java/chapter14/src/com/test/src/Package.java
+++ charlatan/self_study/Java/chapter14/src/com/test/src/Package.java
@@ -1,5 +1,7 @@
 package com.test.src;
 
+import java.util.Scanner;
+
 /**
  * time :2022/5/6 17:03 29
  * ClassName :Package
@@ -11,6 +13,10 @@ package com.test.src;
  */
 public class Package {
     public static void main(String[] args) {
+        /*
+        在根目录下是不用写 package 的，如果在更深的目录中，必须使用 package 进行标注，并且这句话只能出现在源代码的第一行
+        如果在要使用不同文件夹中的内容，需要先导包，才能使用
+         */
         System.out.println("你好，世界");
     }
 }
