669ba03aa7c63cbfde5659334812d9715f7d5471
charlatan <yllyou333@163.com>
2022-05-07 16:06:42 +0800

    二维数组

--- charlatan/self_study/Java/chapter16/src/ArrayTest03.java
+++ charlatan/self_study/Java/chapter16/src/ArrayTest03.java
@@ -15,6 +15,21 @@ public class ArrayTest03 {
          */
         int[] arr1 = {1, 2, 3, 4, 5, 5};
         int[] arr2 = new int[10];
+/*
+        通过 SUN 提供的方法来完成数组的拷贝
+        参数：
+            - 要拷贝的数组
+            - 要拷贝内容的起始位置
+            - 要将数组拷贝到哪个位置
+            - 要将老数组拷贝到哪个数组
+            - 将老数组拷贝到新数组的哪个位置
+            - 要拷贝数组的长度
+ */
+        System.arraycopy(arr1, 0, arr2, 0, arr1.length);
 
+        System.out.println(arr2);
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);
+        }
     }
 }
